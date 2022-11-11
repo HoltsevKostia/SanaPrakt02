c80927a88d97baff4bba1f38762e99ba29621c26
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2022-11-11 18:39:47 +0200

    changed three lines

--- Task1/Task1.cpp
+++ Task1/Task1.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-    std::cout << "Êóğñè Sana Commerce Software Development Bootcamp\n";
-    std::cout << "Êóğñè Sana Commerce Software Development Bootcamp\n";
-    std::cout << "Êóğñè Sana Commerce Software Development Bootcamp\n";
+    std::cout << "new text1\n";
+    std::cout << "new text2\n";
+    std::cout << "new text3\n";
     std::cout << "Êóğñè Sana Commerce Software Development Bootcamp\n";
     std::cout << "Êóğñè Sana Commerce Software Development Bootcamp\n";
     std::cout << "Êóğñè Sana Commerce Software Development Bootcamp\n";
